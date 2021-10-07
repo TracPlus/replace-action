107e9729b83823db8b689fb45e67f20c66d2efa1
Dale Anderson <dale@st8vrt.com>
2021-10-07 16:02:57 +1300

    Cleanups bump node version.

--- index.js
+++ index.js
@@ -1,39 +1,29 @@
 const core = require('@actions/core');
-
-try {
-  const files = core.getInput('files');
-  const vars_string = core.getInput('replacements');
-  const filenames = files.replace(' ', '').split(',');
-  const vars = vars_string.split(',');
-  console.log('files l:'+ filenames.length);
-  for(let fi = 0; fi < filenames.length; fi++)
-  {
-    const filename = filenames[fi];
-    const fs = require('fs');
-    console.log('file1: '+ fi + ' '+filename);
-    fs.readFile(filename, 'utf8', function (err,data) {
-      if (err) {
-        console.log(err);
-      } else {
-        let result = data;
-        console.log(data);
-        for(let i = 0; i < vars.length; i++)
-        {
-          const firstEqual = vars[i].indexOf('=');
-          const key = vars[i].substr(0, firstEqual);
-          const value = vars[i].substr(firstEqual + 1);
-          result = result.replaceAll(key, value)
-        }
-        console.log('file2: '+filename)
-        fs.writeFile(filename, result, 'utf8', function (err) {
-          if (err)
-            console.log(err)
-          else
-            console.log(result)
-        });
+const fs = require("fs").promises;
+async function run() {
+  try {
+    const files = core.getInput('files');
+    const vars_string = core.getInput('replacements');
+    const filenames = files.replace(' ', '').split(',');
+    const vars = vars_string.split(',');
+    console.log('files l:'+ filenames.length);
+    for(const filename of filenames) {
+      const data = await fs.readFile(filename, 'utf8');
+      let result = data;
+      console.log(data);
+      for (let i = 0; i < vars.length; i++) {
+        const firstEqual = vars[i].indexOf('=');
+        const key = vars[i].substr(0, firstEqual);
+        const value = vars[i].substr(firstEqual + 1);
+        result = result.replaceAll(key, value)
       }
-    });
+      console.log('file2: ' + filename)
+      await fs.writeFile(filename, result, 'utf8');
+    }
   }
-} catch (error) {
-  core.setFailed(error.message);
+  catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run()
